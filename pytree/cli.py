045fcec1ff99eef918975fc87ccba91dd21291a4
BrendanMartin <brmartin508@gmail.com>
2024-07-19 13:25:23 -0400

    fnmatch, line styles

--- pytree/cli.py
+++ pytree/cli.py
@@ -1,10 +1,48 @@
 import argparse
 import os
+from dataclasses import dataclass
+from fnmatch import fnmatch
+from typing import Literal
 
-straight = '│'
-tee = '├──'
-elbow = '└──'
-space = ' '
+
+@dataclass
+class StyleNormal():
+    straight: str = '│'
+    tee: str = '├──'
+    elbow: str = '└──'
+    space: str = ' '
+
+
+@dataclass
+class StyleHeavy():
+    straight: str = '┃'
+    tee: str = '┣━━'
+    elbow: str = '┗━━'
+    space: str = ' '
+
+
+@dataclass
+class StyleDouble():
+    straight: str = '║'
+    tee: str = '╠══'
+    elbow: str = '╚══'
+    space: str = ' '
+
+
+styles = {
+    'normal': StyleNormal(),
+    'heavy':  StyleHeavy(),
+    'double': StyleDouble(),
+}
+
+# skip_paths: list[re.Pattern] = [
+#     re.compile(r'(?<![\w\-,_])\.(?!\S*ignore|env).*') # ignore dot files except .<service>ignore and .env
+# ]
+
+skip_patterns = [
+    '*/.*[!ignore]',
+    '*/.[!env]'
+]
 
 
 class Tree:
@@ -15,12 +53,24 @@ class Tree:
         self.children: list[Tree] = []
         self.exclude_dirs = exclude_dirs or []
 
+    def should_skip(self, path):
+        # for skip in skip_paths:
+        #     if skip.search(path):
+        #         return True
+        for skip in skip_patterns:
+            if fnmatch(path, skip):
+                return True
+        for exc in self.exclude_dirs:
+            if fnmatch(path, exc):
+                return True
+        return False
+
     def build(self):
         for name in sorted(os.listdir(self.path)):
             path = os.path.join(self.path, name)
+            if self.should_skip(path):
+                continue
             if os.path.isdir(path):
-                if name in self.exclude_dirs:
-                    continue
                 subtree = Tree(name, path, is_dir=True, exclude_dirs=self.exclude_dirs)
                 self.children.append(subtree)
                 subtree.build()
@@ -28,47 +78,50 @@ class Tree:
                 leaf = Tree(name, path, is_dir=False)
                 self.children.append(leaf)
 
+    def stringify(self, style):
+        def walk(tree: Tree, all_lines: list, line: list):
+            for i, child in enumerate(tree.children):
+                this_line = [*line]
+                next_line = [*line]
+                if i < len(tree.children) - 1:
+                    this_line.append(style.tee)
+                    next_line.extend([style.straight, style.space * 3])
+                else:
+                    this_line.append(style.elbow)
+                    next_line.append(style.space * 4)
 
-def print_tree(tree: Tree):
-    def walk(tree: Tree, line: list):
-        for i, child in enumerate(tree.children):
-            this_line = [*line]
-            next_line = [*line]
-            if i < len(tree.children) - 1:
-                this_line.append(tee)
-                next_line.extend([straight, space * 3])
-            else:
-                this_line.append(elbow)
-                next_line.append(space * 4)
+                if child.is_dir:
+                    this_line.append(f' {child.name}/')
+                    all_lines.append(''.join(this_line))
+                    walk(child, all_lines, next_line)
+                else:
+                    this_line.append(f' {child.name}')
+                    all_lines.append(''.join(this_line))
 
-            if child.is_dir:
-                this_line.append(child.name + '/')
-                print(''.join(this_line))
-                walk(child, next_line)
-            else:
-                this_line.append(child.name)
-                print(''.join(this_line))
-
-    print('\n', tree.name.lstrip('/') + '/')
-    walk(tree, ['  '])
+        all_lines = ['\n' + self.name.lstrip('/') + '/']
+        walk(self, all_lines, ['  '])
+        return '\n'.join(all_lines)
 
 
-def make_tree(dir, exclude_dirs=None):
-    tree = Tree(path=dir, name=dir, is_dir=True, exclude_dirs=exclude_dirs)
+def make_tree(dir, exclude_patterns=None, style: Literal['normal', 'heavy', 'double'] = 'normal'):
+    tree = Tree(path=dir, name=dir, is_dir=True, exclude_dirs=exclude_patterns)
     tree.build()
-    print_tree(tree)
+    style = styles[style]
+    output = tree.stringify(style)
+    return output
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Print directory tree with exclusions")
+    parser = argparse.ArgumentParser(description="Print directory tree")
     parser.add_argument("directory", nargs="?", default=".",
                         help="Directory to start from (default: current directory)")
-    parser.add_argument("-e", "--exclude", nargs="+", default=[], help="Directories to exclude contents from")
+    parser.add_argument("-e", "--exclude", nargs="+", default=[], help="Exclude folders and files using glob patterns")
+    parser.add_argument('-s', '--style', default='normal', choices=['normal', 'heavy', 'double'],
+                        help='The style of lines to draw')
     args = parser.parse_args()
 
-    # print(f"{args.directory}/")
-    # print_tree(args.directory, args.exclude)
-    make_tree(args.directory, args.exclude)
+    output = make_tree(args.directory, args.exclude, args.style)
+    print(output)
 
 
 if __name__ == "__main__":
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,66 @@
 from pytree.cli import make_tree
 
 def test_main(fs):
-    fs.create_file("/var/data/xx1.txt")
-    fs.create_file("/var/data/xx2.txt")
-    fs.create_file("/var/data/more/xx3.txt")
-    fs.create_file("/var/other/yy1.txt")
-    make_tree("/var")
+    exp = """
+ var/
+  ├── .gitignore
+  ├── data/
+  │   ├── more/
+  │   │   └── xx3.txt
+  │   ├── xx1.txt
+  │   └── xx2.txt
+  └── other/
+      └── yy1.txt
+      """.strip()
+
+    fs.create_file('/var/data/xx1.txt')
+    fs.create_file('/var/data/xx2.txt')
+    fs.create_file('/var/data/more/xx3.txt')
+    fs.create_file('/var/other/yy1.txt')
+    fs.create_file('/var/.idea/zz1.txt')
+    fs.create_file('/var/.gitignore')
+    output = make_tree('/var')
+    assert output.strip() == exp
+
+def test_exclude(fs):
+    fs.create_file('/var/data/xx1.txt')
+    fs.create_file('/var/data/xx2.txt')
+    fs.create_file('/var/data/more/xx3.txt')
+    fs.create_file('/var/other/yy1.txt')
+    fs.create_file('/var/other/.idea/foo.bar')
+
+    # Exclude entire folder
+    exclude = ['*/data']
+    exp = """
+var/
+  └── other/
+      └── yy1.txt
+      """.strip()
+    output = make_tree('/var', exclude_patterns=exclude)
+    assert output.strip() == exp
+
+    # Exclude contents but keep folder name
+    exclude = ['*/data/*']
+    exp = """
+var/
+  ├── data/
+  └── other/
+      └── yy1.txt
+      """.strip()
+    output = make_tree('/var', exclude_patterns=exclude)
+    assert output.strip() == exp
+
+
+def test_style(fs):
+    fs.create_file('/var/data/xx1.txt')
+    fs.create_file('/var/other/yy1.txt')
+    exp = """
+var/
+  ╠══ data/
+  ║   ╚══ xx1.txt
+  ╚══ other/
+      ╚══ yy1.txt
+    """.strip()
+    output = make_tree('/var', style='double')
+    assert output.strip() == exp
+
